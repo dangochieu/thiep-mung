2133f49564bcd20bef391b8108f0fa4b8f68b132
dangochieu <89003522+dangochieu@users.noreply.github.com>
2021-10-20 20:21:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Trúc Uyên';
+const nameGirl = '  Ngọc Quyên';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc ny tương lại của mình 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc bạn của mình 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = '245640813_628999798467805_7865362497900209335_n.jpg';
+const giftImage = '242968227_379202370532174_4587061121631650056_n.jpg';
 const base64 = '';
 const giftImageBase64 = "245640813_628999798467805_7865362497900209335_n.jpg " + base64;
