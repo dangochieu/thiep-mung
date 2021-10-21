02601c75a34ae5b5e3f14dfd5cc95886e61d1d34
dangochieu <89003522+dangochieu@users.noreply.github.com>
2021-10-20 19:41:05 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc ny tương lai của mình 20/10 tràn ngập niềm 
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = '245640813_628999798467805_7865362497900209335_n';
+const giftImage = '245640813_628999798467805_7865362497900209335_n.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
